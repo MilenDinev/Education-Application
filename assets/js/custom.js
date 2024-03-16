1487bb4f82cb34b47661236b9c77727652fbe8f7
MilenDinev <dinev.milen@gmail.com>
2024-03-16 23:11:22 +0200

    RF

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,106 +1,88 @@
 (function($) {
+    // Function to add 'js' class to body on document ready
+    function addJsClassToBody() {
+        $('body').addClass('js');
+    }
 
-	$(document).ready(function() {
-	  $('body').addClass('js');
-	  var $menu = $('#menu'),
-	    $menulink = $('.menu-link');
-	  
-	$menulink.click(function() {
-	  $menulink.toggleClass('active');
-	  $menu.toggleClass('active');
-	  return false;
-	});});
+    // Function to toggle menu visibility
+    function toggleMenu() {
+        var $menu = $('#menu'),
+            $menuLink = $('.menu-link');
 
+        $menuLink.click(function() {
+            $menuLink.toggleClass('active');
+            $menu.toggleClass('active');
+            return false;
+        });
+    }
 
-	videoPopup();
+    // Function to initialize Owl Carousel
+    function initializeOwlCarousel() {
+        $('.owl-carousel').owlCarousel({
+            loop: true,
+            margin: 30,
+            nav: true,
+            autoplay: true,
+            autoplayTimeout: 5000,
+            autoplayHoverPause: true,
+            responsive: {
+                0: { items: 1 },
+                550: { items: 2 },
+                750: { items: 3 },
+                1000: { items: 4 },
+                1200: { items: 5 }
+            }
+        });
+    }
 
+    // Function to initialize Slick Slider
+    function initializeSlickSlider() {
+        $(".Modern-Slider").slick({
+            autoplay: true,
+            autoplaySpeed: 10000,
+            speed: 600,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            pauseOnHover: false,
+            dots: true,
+            pauseOnDotsHover: true,
+            cssEase: 'fade',
+            draggable: false,
+            prevArrow: '<button class="PrevArrow"></button>',
+            nextArrow: '<button class="NextArrow"></button>'
+        });
+    }
 
-	$('.owl-carousel').owlCarousel({
-	    loop:true,
-	    margin:30,
-	    nav:true,
-	    autoplay:true,
-		autoplayTimeout:5000,
-		autoplayHoverPause:true,
-	    responsive:{
-	        0:{
-	            items:1
-	        },
-	        550:{
-	            items:2
-	        },
-	        750:{
-	            items:3
-	        },
-	        1000:{
-	            items:4
-	        },
-	        1200:{
-	            items:5
-	        }
-	    }
-	})
+    // Function to toggle visibility of hidden content on hover
+    function toggleHiddenContentOnHover() {
+        $("div.features-post").hover(
+            function() {
+                $(this).find("div.content-hide").slideToggle("medium");
+            },
+            function() {
+                $(this).find("div.content-hide").slideToggle("medium");
+            }
+        );
+    }
 
+    // Function to initialize jQuery UI Tabs
+    function initializeJQueryUITabs() {
+        $("#tabs").tabs();
+    }
 
-	$(".Modern-Slider").slick({
-	    autoplay:true,
-	    autoplaySpeed:10000,
-	    speed:600,
-	    slidesToShow:1,
-	    slidesToScroll:1,
-	    pauseOnHover:false,
-	    dots:true,
-	    pauseOnDotsHover:true,
-	    cssEase:'fade',
-	   // fade:true,
-	    draggable:false,
-	    prevArrow:'<button class="PrevArrow"></button>',
-	    nextArrow:'<button class="NextArrow"></button>', 
-	});
+    // Main function to initialize all functionalities
+    function initialize() {
+        addJsClassToBody();
+        toggleMenu();
+        initializeOwlCarousel();
+        initializeSlickSlider();
+        toggleHiddenContentOnHover();
+        initializeJQueryUITabs();
+    }
 
-
-	$("div.features-post").hover(
-	    function() {
-	        $(this).find("div.content-hide").slideToggle("medium");
-	    },
-	    function() {
-	        $(this).find("div.content-hide").slideToggle("medium");
-	    }
-	 );
-
-
-	$( "#tabs" ).tabs();
-
-
-	(function init() {
-	  function getTimeRemaining(endtime) {
-	    var t = Date.parse(endtime) - Date.parse(new Date());
-	    var seconds = Math.floor((t / 1000) % 60);
-	    var minutes = Math.floor((t / 1000 / 60) % 60);
-	    var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
-	    var days = Math.floor(t / (1000 * 60 * 60 * 24));
-	    return {
-	      'total': t,
-	      'days': days,
-	      'hours': hours,
-	      'minutes': minutes,
-	      'seconds': seconds
-	    };
-	  }
-	  
-	  function initializeClock(endtime){
-	  var timeinterval = setInterval(function(){
-	    var t = getTimeRemaining(endtime);
-	    document.querySelector(".days > .value").innerText=t.days;
-	    document.querySelector(".hours > .value").innerText=t.hours;
-	    document.querySelector(".minutes > .value").innerText=t.minutes;
-	    document.querySelector(".seconds > .value").innerText=t.seconds;
-	    if(t.total<=0){
-	      clearInterval(timeinterval);
-	    }
-	  },1000);
-	}
-	initializeClock(((new Date()).getFullYear()+1) + "/1/1")
-	})()
+    // Execute the initialization function on document ready
+    $(document).ready(function() {
+        initialize();
+    });
 
 })(jQuery);
